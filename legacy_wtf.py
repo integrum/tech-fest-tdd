1c0486d5234284aec4a90bcaa109b2953a9d214b
Clayton Lengel-Zigich <clayton@claytonlz.com>
2014-05-09 23:02:22 -0700

    legacy tests

--- legacy_wtf.py
+++ legacy_wtf.py
@@ -1,15 +1,31 @@
 from legacy import SuggestionEngine
+import unittest
 
-s = SuggestionEngine()
+engine = SuggestionEngine()
 
-# print s.suggest() exception
-print s.suggest("","","","")
-print s.suggest(None, None, None, None)
-print s.suggest(0,1,2,3)
-print s.suggest(9223372036854775808L,9223372036854775808L,9223372036854775808L,9223372036854775808L)
-print s.suggest(-9223372036854775808L,-9223372036854775808L,-9223372036854775808L,-9223372036854775808L)
-print s.suggest("Casino","","","")
-print s.suggest("Casino","niro","","")
-print s.suggest("Casino","Stone","","")
-print s.suggest("Casino","Stone",9,"")
-print s.suggest("Casino","Stone",3,"")
+# print engine.suggest() exception
+print engine.suggest("","","","")
+print engine.suggest(None, None, None, None)
+print engine.suggest(0,1,2,3)
+print engine.suggest(9223372036854775808L,9223372036854775808L,9223372036854775808L,9223372036854775808L)
+print engine.suggest(-9223372036854775808L,-9223372036854775808L,-9223372036854775808L,-9223372036854775808L)
+print engine.suggest("Casino","","","")
+print engine.suggest("Casino","niro","","")
+print engine.suggest("Casino","Stone","","")
+print engine.suggest("Casino","Stone",0,"")
+print engine.suggest("Casino","Stone",1,"")
+print engine.suggest("Casino","Stone",2,"")
+print engine.suggest("Casino","Stone",3,"")
+print engine.suggest("Casino","Stone",4,"")
+# ...
+
+
+class TestKnownKnowns(unittest.TestCase):
+
+  def setUp(self):
+    self.engine = SuggestionEngine()
+
+  def test_casino_stone_3_blank(self):
+    self.assertEqual(self.engine.suggest("Casino","Stone",3,""), "De Palma")
+
+  # ...
